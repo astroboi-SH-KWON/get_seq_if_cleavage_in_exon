b1f699dfbbf579377508c1f2d8539965cd635515
astroboi-lab <terry007x@korea.ac.kr>
2020-09-04 13:45:52 +0900

    done

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ PAM = 'NNGRRT'
 LEN_AFTR_PAM = 3
 INIT_0 = [LEN_SPACER, CLEAVAGE, PAM, LEN_AFTR_PAM]
 INIT_1 = [41, CLEAVAGE, "NNRGAA", LEN_AFTR_PAM]
-INIT = [INIT_0, INIT_0, INIT_1]
+INIT_RULE = [INIT_0, INIT_0, INIT_1]
 ############### end setting env #################
 
 def main():
@@ -23,11 +23,12 @@ def main():
     logic_prep = LogicPrep.LogicPreps()
     logic = Logic.Logics()
 
-    for file_idx in range(len(INIT)):
+    for file_idx in range(len(INIT_RULE)):
         seq_record = util.get_seq_record_from_genbank(WORK_DIR + NCBI + FILE_NAME[file_idx] + ".gb")
         cds_idx_list = logic_prep.get_cds_idx_arr_to_list(seq_record)
 
-        pam_seq = INIT[file_idx][2]
+        init_rule = INIT_RULE[file_idx]
+        pam_seq = init_rule[2]
         plus_strand_list, minus_strand_list = logic.get_idx_of_matching_seq(seq_record.seq, pam_seq)
 
         plus_idx_list = logic.get_idx_in_list(plus_strand_list, cds_idx_list)
@@ -36,8 +37,8 @@ def main():
         filtered_plus_idx_list = logic_prep.filter_out_dupl(plus_idx_list)
         filtered_minus_idx_list = logic_prep.filter_out_dupl(minus_idx_list)
 
-        plus_seq_list = logic.get_trgt_seq_in_idx_list(seq_record.seq, filtered_plus_idx_list, INIT)
-        minus_seq_list = logic.get_trgt_seq_in_idx_list(seq_record.seq, filtered_minus_idx_list, INIT, False)
+        plus_seq_list = logic.get_trgt_seq_in_idx_list(seq_record.seq, filtered_plus_idx_list, init_rule)
+        minus_seq_list = logic.get_trgt_seq_in_idx_list(seq_record.seq, filtered_minus_idx_list, init_rule, False)
 
         merge_list = logic_prep.merge_list([plus_seq_list, minus_seq_list])
         tot_list = logic_prep.sort_list_by_ele(merge_list, 0)
